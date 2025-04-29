869f4269be5dff6d04f3188a690c7551d373542b
Matheus Felipe <matheusfelipedevv@gmail.com>
2025-04-29 12:44:10 -0300

    a

--- src/greenvolt/__init__.py
+++ src/greenvolt/__init__.py
@@ -1,25 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_wtf.csrf import CSRFProtect
 import psycopg2
-from datetime import datetime
 
-app = Flask(__name__)
 db = SQLAlchemy()
 login_manager = LoginManager()
-app.config['SECRET_KEY'] = 'a5881f26bae925d752a97795'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:PML1RRjpQ8foE5y1@proudly-unbiased-spadefish.data-1.use1.tembo.io:5432/postgres'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
-db.init_app(app)
-bcrypt = Bcrypt(app)
-login_manager.init_app(app)
-csrf = CSRFProtect(app)
-login_manager.login_view = "page_login"
-login_manager.login_message = "Por favor, realize o login"
-login_manager.login_message_category = "info"
+bcrypt = Bcrypt()
+csrf = CSRFProtect()
 
+def create_app():
+    app = Flask(__name__)
+    
+    app.config['SECRET_KEY'] = 'a5881f26bae925d752a97795'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:PML1RRjpQ8foE5y1@proudly-unbiased-spadefish.data-1.use1.tembo.io:5432/postgres'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
+    
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    csrf.init_app(app)
+    
+    login_manager.login_view = "page_login"
+    login_manager.login_message = "Por favor, realize o login"
+    login_manager.login_message_category = "info"
 
-
-from greenvolt import routes
+    from greenvolt import routes  # importa aqui dentro da função
+    return app
